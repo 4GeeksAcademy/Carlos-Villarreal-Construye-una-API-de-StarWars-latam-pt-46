c6b5629bb41394979e9e7523e379dffe8648dbce
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-05-03 04:14:50 +0000

    Creando los endpoints

--- src/models.py
+++ src/models.py
@@ -4,12 +4,25 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class People(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    firt_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(255), nullable=False)
 
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    picture: Mapped[str] = mapped_column(String(), nullable=True)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(db.Integer, db)
 
     def serialize(self):
         return {
